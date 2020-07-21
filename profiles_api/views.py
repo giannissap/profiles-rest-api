c27ea2ae7a0b0804b67ee643e67118740ffce578
John <giannis_sap@hotmail.com>
2020-07-21 13:16:54 +0300

    Added APIView

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+
+class HelloApiView(APIView):
+    """Test API View"""
+
+    def get(self, request, format=None):
+        """Returns a list(dictionairy) of APIView feautures"""
+        an_apiview=[
+            'Lalalalala',
+            'Hello',
+            'John',
+        ]
+
+        return Response({'message':'Hello World','an_apiview':an_apiview})
--- profiles_project/urls.py
+++ profiles_project/urls.py
@@ -14,8 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include #include -> u can include other  urls from other project folders ( profiles_api ) to the root project ( profiles_project )
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include('profiles_api.urls')),
 ]
